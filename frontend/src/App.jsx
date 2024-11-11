7aff487e0bff80f856fd4341e2a0300338a075cf
e41c <le41cl@gmail.com>
2024-11-11 14:51:55 -0500

    built parent folder, front-end folder, and back-end folder. Also created node.js server, and react.js project

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+// src/App.jsx
+function App() {
+  return (
+    <div className="bg-blue-500 text-white p-6 rounded-lg shadow-lg">
+      <h1 className="text-2xl font-bold">Hello, Tailwind CSS!</h1>
+      <p className="mt-2">This is a simple Tailwind CSS example.</p>
+    </div>
+  );
+}
+
+export default App;
+
